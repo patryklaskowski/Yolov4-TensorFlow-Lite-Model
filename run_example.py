8066b8b0b5327c31baf6273a465a44b7ef0b318e
Patryk Laskowski <patryk.jacek.laskowski@gmail.com>
2021-01-12 14:39:05 +0100

    Update run_example.py

--- run_example.py
+++ run_example.py
@@ -2,16 +2,14 @@ from TensorFlowLiteModel import create_prediction_stream
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-tf', '--tflite', dest = 'tflife', default = './checkpoints/yolov4-coco-416.tflite', help = 'Path to tfite model.')
+parser.add_argument('-tf', '--tflite', dest = 'tflite', default = './checkpoints/yolov4-coco-416.tflite', help = 'Path to tfite model.')
 parser.add_argument('-n', '--names', dest = 'names', default = './data/classes/coco.names', help = 'Path to .names file.')
+parser.add_argument('-s', '--size', dest = 'size', default = 416, help = 'Input size', type=int)
 args = parser.parse_args()
 
 def main():
-    model_path = './checkpoints/yolov4-coco-416.tflite'
-    names_path = './data/classes/coco.names'
     video_source = 0 #'http://208.139.200.133/mjpg/video.mjpg' # 0
-#     create_prediction_stream(model_path, names_path, video_source)
-    create_prediction_stream(args.tflite, args.names, video_source)
+    create_prediction_stream(args.tflite, args.names, video_source, input_size=args.size)
 
 if __name__ == '__main__':
     main()
